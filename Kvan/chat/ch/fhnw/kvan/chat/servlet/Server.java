028da13c8fc55147d30bd9f3ff2c44292ca26e8c
Joachim Leu <jl@layzapp.com>
2014-06-03 13:38:29 +0200

    ok

--- Kvan/chat/ch/fhnw/kvan/chat/servlet/Server.java
+++ Kvan/chat/ch/fhnw/kvan/chat/servlet/Server.java
@@ -1,9 +1,30 @@
 package ch.fhnw.kvan.chat.servlet;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch.fhnw.kvan.chat.ChatRoomDriver;
 
 public class Server extends HttpServlet {
-
 	private static final long serialVersionUID = 2659407803062419933L;
+	
+	public static void main(String args[]) throws IOException {
+		int port = 1235;
+		ChatRoomDriver crd = new ChatRoomDriver();
+		crd.connect("localhost", port);
+		
+		}
+	
+	public void service(HttpServletRequest req, HttpServletResponse res){
+		String method= req.getMethod();
+		
+		
+		
+	}
+	
+	
 
 }
