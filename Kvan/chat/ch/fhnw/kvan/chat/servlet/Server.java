fd2bf61a9a680df9fe1becdfeb42a12ee8f334d4
mambax <dmnkmyr@gmail.com>
2014-06-05 16:24:45 +0200

    jkl

--- Kvan/chat/ch/fhnw/kvan/chat/servlet/Server.java
+++ Kvan/chat/ch/fhnw/kvan/chat/servlet/Server.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ch.fhnw.kvan.chat.ChatRoom;
 import ch.fhnw.kvan.chat.ChatRoomDriver;
 
 @WebServlet("/Kvan/chat/Server")
@@ -45,31 +46,64 @@ public class Server extends HttpServlet {
 			System.out.println(params[i]);
 			i++;
 		}
-		String rep = helper.getResponse(method, params);
-		System.out.println(rep);
-		byteOut.write(rep.getBytes());
+		ServletOutputStream sOut;
+		if (!(method.equals("refresh"))) {
+			String rep = helper.getResponse(method, params);
+			System.out.println(rep);
+			byteOut.write(rep.getBytes());
 
-		byte[] buf = byteOut.toByteArray();
-		response.setContentType("application/octet-stream");
-		response.setContentLength(buf.length);
-		System.out.println("now writing");
-		ServletOutputStream sOut = response.getOutputStream();
-		sOut.write(buf);
-		sOut.close();
+			byte[] buf = byteOut.toByteArray();
+			response.setContentType("application/octet-stream");
+			response.setContentLength(buf.length);
+			System.out.println("now writing");
+			sOut = response.getOutputStream();
+			sOut.write(buf);
+			sOut.close();
+		} else {
+			System.out.println("Refresh!!!!! ------ - - -- - - - - - - - -  -");
+			String topic = "";
+			topic = params[0];
+			sOut = response.getOutputStream();
+			if (!topic.equalsIgnoreCase("")) {
+				String messages = crd.getChatRoom().refresh(topic) + "\r\n";
+				if (!messages.equalsIgnoreCase("")) {
+					System.out.println("writing messages: " + messages);
+					byteOut.write(messages.getBytes());
+					byte[] msgs = byteOut.toByteArray();
+					sOut.write(msgs);
+				}
+			}
+			String participants = ((ChatRoom) crd.getChatRoom())
+					.getParticipants() + "\r\n";
+			System.out.println("writing participants: " + participants);
+			byteOut.write(participants.getBytes());
+			byte[] msgs = byteOut.toByteArray();
+			sOut.write(msgs);
+			sOut.flush();
+			String topics = ((ChatRoom) crd.getChatRoom()).getTopics() + "\r\n";
+			System.out.println("writing topics: " + topics);
+			byteOut.write(topics.getBytes());
+			msgs = byteOut.toByteArray();
+			sOut.write(msgs);
+			sOut.close();
+		}
+
 	}
 
 	static class ServerHelper {
+		private boolean success;
 
 		private String getResponse(String method, String[] params)
 				throws IOException {
 
 			switch (method) {
 			case "addParticipant":
-				System.out.println("addParticipant, name: " + params[0]);
-				if (crd.getChatRoom().addParticipant(params[0]))
-					return "add_participant=" + params[0] + "\r\n";
-				else
-					return "null";
+				success = false;
+				String clientname = params[0];
+				if (!clientname.equalsIgnoreCase("")) {
+					success = crd.getChatRoom().addParticipant(clientname);
+				}
+				return "add_paticipant=" + clientname + "\r\n";
 
 			case "removeParticipant":
 				System.out.println("removeParticipant, name: " + params[0]);
@@ -100,22 +134,17 @@ public class Server extends HttpServlet {
 					return null;
 			case "getMessages":
 				System.out.println("getMessage, message: " + params[0]);
-				if (crd.getChatRoom().getMessages(params[0]) != null)
-					return "messages=" + params[0] + "\r\n";
+				String res = crd.getChatRoom().getMessages(params[0]);
+				if (res != null)
+					return res + "\r\n";
 				else
 					return null;
-
-			case "refresh":
-				System.out.println("refresh, topic: " + params[0]);
-				return crd.getChatRoom().refresh(params[0]);
-
 			default:
 				System.out.println("False GET request");
 				throw new RuntimeException();
 
 			}
 		}
-
 	}
 
 }
